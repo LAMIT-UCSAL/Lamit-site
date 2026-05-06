470554c7480ec8e054cb3fd65a77699abd87501e
Silva, Guilherme (G.M.) <monjogui@gmail.com>
2026-05-06 14:12:06 -0300

    update: visual improvements in "Diretoria" page

--- src/components/DirectorBanner.jsx
+++ src/components/DirectorBanner.jsx
@@ -1,10 +1,22 @@
-function DirectorBanner({ index, label, name, description, image, reverse }) {
+import TiltedCard from "./TiltedCard";
+
+function DirectorBanner({ index, label, name, description, experience, image, reverse }) {
   const className = `director-banner${reverse ? ' director-banner--reverse' : ''}`
 
   return (
     <article className={className}>
       <div className="director-banner__media">
-        <img src={image} alt={name} />
+       <TiltedCard
+        imageSrc={image}
+        altText={name}
+        captionText={experience}
+        containerHeight="100%"
+        containerWidth="100%"
+        imageHeight="100%"
+        imageWidth="100%"
+        showMobileWarning={false}
+        showTooltip={true}
+      />
       </div>
       <div className="director-banner__content">
         <span className="director-banner__index">{index}</span>
